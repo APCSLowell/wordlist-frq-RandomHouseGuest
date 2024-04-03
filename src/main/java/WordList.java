260b6fb73003c76d1813ec5b4a52fb234b3a7dc4
Kyle Yuen <103063168+RandomHouseGuest@users.noreply.github.com>
2024-04-03 09:36:47 -0700

    WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -8,15 +8,24 @@ public class WordList
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    /* to be implemented in part (a) */
-    
+  public int numWordsOfLength(int len){
+    int count = 0;
+    for(int i = 0; i < myList.size(); i++){
+      if(myList.get(i).length() == len){
+        count++;
+      }
+    }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+   int i = 0;
+    while(i < myList.size()){
+      if(myList.get(i).length() == len){
+        myList.remove(i);
+      } else{
+        i++;
+      }
   }
 }
